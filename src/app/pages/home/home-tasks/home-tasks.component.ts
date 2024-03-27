67a83e48132d48a59907e2500701df79af28831e
Random4eg <panaram452@gmail.com>
2024-03-27 17:26:02 +0200

    angular

---
+++ src/app/pages/home/home-tasks/home-tasks.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-tasks',
+  templateUrl: './home-tasks.component.html',
+  styleUrl: './home-tasks.component.scss'
+})
+export class HomeTasksComponent {  public dailly = [
+
+  {
+    id: Date.now(),
+    title: 'Short news title will be here',
+  }
+]
+
+}
